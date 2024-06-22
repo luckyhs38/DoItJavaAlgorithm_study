769279c8fe076ee69b0341fc2d9d8fc340238460
Lee HyunSeung <luckyhs38@daum.net>
2024-06-23 00:15:52 +0900

    20240623

---
+++ 연습문제_예제소스/chap01EX/10/DigitsNo_i.java
@@ -0,0 +1,15 @@
+// 연습1-10
+// 양의 정숫값의 자릿수를 구하여 출력
+
+import java.util.Scanner;
+
+class DigitsNo_i {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("양의 정수를 입력하세요 : "); 
+		int i = sc.nextInt();
+		
+
+	}
+}
